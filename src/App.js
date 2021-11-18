b472bf37e66fcd505a955bf656c13172500e178a
Dimitrii Vinogradov <siteddv@gmail.com>
2021-11-19 01:13:43 +0600

    added redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setBooks } from './actions/books';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { books } = this.props.booksReducer;
+    const { setBooks } = this.props;
+    const newBooks = [
+      {
+        id: 0,
+        title: 'Shopping Cart ' + new Date()
+      }
+    ];
+    return (
+      <div>
+        <h1>{books[0].title}</h1>
+        <button onClick={setBooks.bind(this, newBooks)}>Set new book</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  setBooks: books => dispatch(setBooks(books))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/reducers/books.js
@@ -0,0 +1,28 @@
+const initialState = {
+   books: [
+      {
+         id: 0,
+         title: "Hello World!"
+      }
+   ]
+}
+
+export default (state = initialState, action) => {
+   switch (action.type) {
+      case 'SET_BOOKS':
+         return {
+            ...state,
+            books: action.payload
+         };
+      case 'ADD_BOOKS':
+         return {
+            ...state,
+            books: [
+               ...state.books,
+               action.payload
+            ]
+         };
+      default:
+         return state;
+   }
+};
