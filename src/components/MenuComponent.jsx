1d9cabbc921f19997073c13e0677363ceee3f059
Dimitrii Vinogradov <siteddv@gmail.com>
2021-11-22 00:31:20 +0600

    added deleting books from cart

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Popup, List, Button, Image } from "semantic-ui-react";
 
-const MenuComponent = ({ totalPrice, count }) => (
+const CartComponent = ({ title, id, image, removeFromCart }) => (
+   <List divided verticalAlign='middle'>
+      <List.Item >
+         <List.Content floated='right'>
+            <Button onClick={removeFromCart.bind(this, id)} color="red">
+               Delete
+            </Button>
+         </List.Content>
+         <Image avatar src={image} />
+         <List.Content>{title}</List.Content>
+      </List.Item>
+   </List>
+);
+
+const MenuComponent = ({ totalPrice, count, items }) => (
    <Menu>
       <Menu.Item name='browse'>
          Books store
@@ -12,9 +26,20 @@ const MenuComponent = ({ totalPrice, count }) => (
             Total: &nbsp; <b>{totalPrice}</b> &nbsp; USD.
          </Menu.Item>
 
-         <Menu.Item name='help'>
-            Cart (<b>{count}</b>)
-         </Menu.Item>
+         <Popup
+            trigger={
+               <Menu.Item name='help'>
+                  Cart (<b>{count}</b>)
+               </Menu.Item>
+            }
+            content={items.length > 0
+               ? items.map(book => <CartComponent key={book.id} {...book} />)
+               : "There is nothing in cart"
+            }
+            on="click"
+            hideOnScroll
+         />
+
       </Menu.Menu>
    </Menu>
 );
--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -2,10 +2,12 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as cartActions from '../actions/cart';
 import Menu from '../components/MenuComponent';
+import uniqBy from 'lodash/uniqBy';
 
 const mapStateToProps = ({ cartReducer }) => ({
    totalPrice: cartReducer.items.reduce((total, book) => total + book.price, 0),
-   count: cartReducer.items.length
+   count: cartReducer.items.length,
+   items: uniqBy(cartReducer.items, c => c.id)
 })
 
 const mapDispatchToProps = dispatch => ({
